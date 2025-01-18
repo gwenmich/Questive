74f8c3c39677446b9bff2ab985e66314e4c51abf
gwenmich <79330147+cgmich@users.noreply.github.com>
2025-01-18 15:16:16 +0000

    Add colours and get choices

--- src/game_config/global_config.py
+++ src/game_config/global_config.py
@@ -8,4 +8,6 @@ FONT = "/assets/font/PressStart2P-Regular.ttf"
 SMALL_FONT = pygame.font.Font(FONT, 20)
 LARGE_FONT = pygame.font.Font(FONT, 30)
 
-
+WHITE = "#FFFFFF"
+BLACK = "#000000"
+DARK_GREY = "#424242"
--- src/screens/questions.py
+++ src/screens/questions.py
@@ -1,6 +1,8 @@
 from src.game_config.utils import render_text
 from src.game_config.global_config import *
 from src.api import questions
+import random
+from src.game_config.button import Button
 
 class Question:
     def __init__(self, display, game_state_manager):
@@ -9,10 +11,16 @@ class Question:
 
     def display_question(self):
         self.display.fill("black")
-        render_text(questions[0]["question"], SMALL_FONT, (1280//2, 180), self.display)
+        render_text(questions[0]["question"], SMALL_FONT, (SCREEN_WIDTH//2, 180), self.display)
 
     def display_choices(self):
-        questions[0][]
+        choices = []
+        questions[0]["correct_answer"].append(choices)
+        for ans in questions[0]["incorrect_answers"]:
+            ans.append(choices)
+
+        first_choice = Button(SCREEN_WIDTH//2, 220,800, 40, WHITE)
+
 
 
     def run(self):

a548ec3828b6736ec95fbc39745d4544d8bb5a21
gwenmich <79330147+cgmich@users.noreply.github.com>
2025-01-18 15:06:49 +0000

    Button logic

---
+++ src/game_config/button.py
@@ -0,0 +1,33 @@
+import pygame
+from src.game_config.global_config import *
+
+class Button:
+
+    def __init__(self, x, y, width, height, colour, bg_colour, text):
+        self.font = SMALL_FONT
+        self.text = text
+
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+        self.colour = colour
+        self.bg_colour = bg_colour
+
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill(self.bg_colour)
+        self.rect = self.image.get_rect(topleft = (self.x, self.y))
+
+        self.content = self.font.render(self.text, True, self.colour)
+        self.content_rect = self.content.get_rect(center = (self.width / 2, self.height / 2))
+        self.image.blit(self.content, self.content_rect)
+
+
+    def is_pressed(self, position, pressed):
+        if self.rect.collidepoint(position):
+            if pressed[0]:
+                return True
+            else:
+                return False
+        return False
--- src/screens/questions.py
+++ src/screens/questions.py
@@ -11,6 +11,9 @@ class Question:
         self.display.fill("black")
         render_text(questions[0]["question"], SMALL_FONT, (1280//2, 180), self.display)
 
+    def display_choices(self):
+        questions[0][]
+
 
     def run(self):
         self.display_question()

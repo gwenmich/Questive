a33ddce611f613e062c4126a8ddd426d4c81990a
Melanie Clark <melaniefclark@gmail.com>
2025-01-24 22:40:58 +0000

    rules.py button and functionality added

--- src/screens/rules.py
+++ src/screens/rules.py
@@ -1,13 +1,19 @@
-from src.game_config.global_config import MEDIUM_FONT, SCREEN_WIDTH, SCREEN_HEIGHT
+from src.game_config.button import Button
+from src.game_config.global_config import MEDIUM_FONT, SCREEN_WIDTH, SMALL_BUTTON_HEIGHT, LARGE_BUTTON_WIDTH
 from src.screens.base_screen import BaseScreen
 
 
 class Rules(BaseScreen):
+    def __init__(self, display, game_state_manager, draw, timer):
+        super().__init__(display, game_state_manager, draw)
+        self.timer = timer
+        self.start_investigation_button = Button(SCREEN_WIDTH // 2 - LARGE_BUTTON_WIDTH // 2, 625, LARGE_BUTTON_WIDTH,
+                                                 SMALL_BUTTON_HEIGHT,
+                                                 "Start investigation...", MEDIUM_FONT)
 
     def draw_screen(self):
-
         text = [
-            "It was supposed to be a fun Quiz Night in Trivia Hollow.,",
+            "It was supposed to be a fun Quiz Night in Trivia Hollow.",
             "But when the Quizmaster was found dead backstage, the night ",
             "turned into a deadly game of wits. The suspects? Everyone in ",
             "the pub—from the trivia-obsessed know-it-all to the mysterious",
@@ -22,14 +28,23 @@ class Rules(BaseScreen):
             "Or will you be remembered ",
             "as the player who didn’t know their capital cities?",
             "",
-            "Press ENTER to continue"
+            "Trivia Hollow needs you!"
         ]
 
         self.draw.render_text_multiple_lines(text)
 
+    def draw_button(self):
+        self.display.blit(self.start_investigation_button.image, self.start_investigation_button.rect)
+
+    def check_button_press(self):
+        if self.start_investigation_button.is_pressed():
+            self.game_state_manager.set_state("question")
+            self.timer.start_timer()
 
     def run(self):
         self.draw_screen()
+        self.draw_button()
+        self.check_button_press()
 
 
 if __name__ == "__main__":

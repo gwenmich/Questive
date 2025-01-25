6734364fa0691df29da63c59b3500ac9d07e2a91
gwenmich <79330147+cgmich@users.noreply.github.com>
2025-01-25 20:26:18 +0000

    Split text for long text in Q&As

--- src/api.py
+++ src/api.py
@@ -2,9 +2,25 @@ import requests
 import html
 
 
-endpoint = "https://opentdb.com/api.php?amount=10&category=9"
+# class ApiData:
+
+    # def __init__(self):
+    #     self.endpoint = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy"
+    #     self.response = requests.get(self.endpoint)
+    #     self.data = self.response.json()
+
+endpoint = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy"
 response = requests.get(endpoint)
 data = response.json()
+
+
+def decode_strings():
+    for question in data["results"]:
+        question["question"] = html.unescape(question["question"])
+        question["correct_answer"] = html.unescape(question["correct_answer"])
+        question["incorrect_answers"] = [html.unescape(ans) for ans in question["incorrect_answers"]]
+    return data
+
 
 # test data
 # data = {
@@ -13,8 +29,8 @@ data = response.json()
 #         {'type': 'multiple',
 #          'difficulty': 'medium',
 #          'category': 'General Knowledge',
-#          'question': 'What is the Swedish word for &quot;window&quot;?',
-#          'correct_answer': 'F&ouml;nster',
+#          'question': 'What is the Swedish word for &quot;window&quot; and what is the most famous swedish outlet for buying furniture?',
+#          'correct_answer': 'F&ouml;nster which is the equivalent of window in swedish and I love Ikea',
 #          'incorrect_answers': ['H&aring;l', 'Sk&auml;rm', 'Ruta']},
 #
 #         {'type': 'boolean',
@@ -25,14 +41,6 @@ data = response.json()
 #          'incorrect_answers': ['True']}
 #     ]
 # }
-
-def decode_strings():
-
-    for question in data["results"]:
-        question["question"] = html.unescape(question["question"])
-        question["correct_answer"] = html.unescape(question["correct_answer"])
-        question["incorrect_answers"] = [html.unescape(ans) for ans in question["incorrect_answers"]]
-    return data
 
 
 if __name__ == "__main__":

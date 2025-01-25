e132307e1efd5e23c63c35a68f8e3aeed83b3c50
gwenmich <79330147+cgmich@users.noreply.github.com>
2025-01-25 21:39:39 +0000

    Logo added

--- src/screens/main_menu.py
+++ src/screens/main_menu.py
@@ -1,3 +1,5 @@
+import pygame.image
+
 from src.screens.base_screen import BaseScreen
 from src.game_config.utils import *
 
@@ -6,13 +8,16 @@ class MainMenu(BaseScreen):
 
     def __init__(self, display, game_state_manager, draw):
         super().__init__(display, game_state_manager, draw)
+        self.logo_img = pygame.image.load("assets/questive_logo.png")
         self.play_button = Button(SCREEN_WIDTH // 2 - MEDIUM_BUTTON_WIDTH // 2, 360, MEDIUM_BUTTON_WIDTH,
                                   MEDIUM_BUTTON_HEIGHT, "Play", LARGE_FONT)
         self.exit_button = Button(SCREEN_WIDTH // 2 - MEDIUM_BUTTON_WIDTH // 2, 490, MEDIUM_BUTTON_WIDTH,
                                   MEDIUM_BUTTON_HEIGHT, "Exit", LARGE_FONT)
 
     def draw_screen(self):
-        # to add logo of game above buttons
+
+        self.display.blit(self.logo_img, (SCREEN_WIDTH//2 - 292, 150))
+
         self.display.blit(self.play_button.image, self.play_button.rect)
         self.display.blit(self.exit_button.image, self.exit_button.rect)
 

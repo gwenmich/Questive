70a45b293051af93cdfe3bcfdfefbe33bf350f7d
gwenmich <79330147+cgmich@users.noreply.github.com>
2025-01-26 13:52:17 +0000

    Game lost text

--- src/screens/game_lost.py
+++ src/screens/game_lost.py
@@ -5,9 +5,11 @@ from src.screens.base_screen import BaseScreen
 class GameLost(BaseScreen):
 
     def draw_screen(self):
-        # ------ Temporary placeholder----- ----------
-        text = "You lose"
-        self.draw.render_text(text, MEDIUM_FONT, (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+        text = ["You didn't manage to uncover the killer in time...",
+                "They struck again and you were the target.",
+                "Let's hope someone else finds them..."
+                ]
+        self.draw.render_text_multiple_lines(text, MEDIUM_FONT, (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
 
     def run(self):
         self.draw_screen()

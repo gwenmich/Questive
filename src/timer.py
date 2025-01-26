32b7498e15b4e174042178d2ed6b5f582e68fb76
Melanie Clark <melaniefclark@gmail.com>
2025-01-26 13:39:13 +0000

    timer.py getter and setters added for game_won

--- src/timer.py
+++ src/timer.py
@@ -6,19 +6,30 @@ class Timer:
     def __init__(self, draw):
         self.draw = draw
         self.start_time = None
+        self.current_time = ""
+        self.timer_running = False
 
     # starts the timer when called
     def start_timer(self):
         self.start_time = time.time()
+        self.timer_running = True
 
     # draws time remaining to screen counting up
     def draw_timer(self):
-        elapsed_time = time.time() - self.start_time
-        seconds = int(elapsed_time % 60)
-        minutes = seconds // 60  # rounds down to nearest integer
+        if self.timer_running:
+            elapsed_time = time.time() - self.start_time
+            seconds = int(elapsed_time % 60)
+            minutes = seconds // 60  # rounds down to nearest integer
+            self.current_time = f"{minutes:02}:{seconds:02}"
+            # displays timer with 2 digits for minutes and seconds
+        self.draw.render_text(str(self.current_time), MEDIUM_FONT, (100, 50))
+        self.set_time(self.current_time)
 
-        # displays timer with 2 digits for minutes and seconds
-        self.draw.render_text(f"{minutes:02}:{seconds:02}", MEDIUM_FONT, (100, 50))
+    def set_time(self, current_time):
+        self.current_time = current_time
+
+    def get_time(self):
+        return self.current_time
 
 
 if __name__ == "__main__":

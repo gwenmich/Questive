ef7fa275f7d74e0de546f8fbd22c5611489159eb
gwenmich <79330147+cgmich@users.noreply.github.com>
2025-01-26 14:58:05 +0000

    Larger game lost image and render

--- src/screens/game_lost.py
+++ src/screens/game_lost.py
@@ -1,15 +1,18 @@
 from src.game_config.global_config import MEDIUM_FONT, SCREEN_WIDTH, SCREEN_HEIGHT
 from src.screens.base_screen import BaseScreen
+import pygame
 
 
 class GameLost(BaseScreen):
 
     def draw_screen(self):
+        img = pygame.image.load("assets/game_lost.png")
+        self.display.blit(img, (SCREEN_WIDTH // 2 - 128, 350))
         text = ["You didn't manage to uncover the killer in time...",
                 "They struck again and you were the target.",
                 "Let's hope someone else finds them..."
                 ]
-        self.draw.render_text_multiple_lines(text, MEDIUM_FONT, (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+        self.draw.render_text_multiple_lines(text)
 
     def run(self):
         self.draw_screen()
